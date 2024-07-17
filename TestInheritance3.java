b3170532d8dd11190490258d02fcda3e169f2f27
SSriyaReddy <168445860+SSriyaReddy@users.noreply.github.com>
2024-07-17 12:21:52 +0530

    Add files via upload

---
+++ TestInheritance3.java
@@ -0,0 +1,11 @@
+package com.day2;
+
+public class TestInheritance3 {
+	public static void main(String args[]){  
+		C c=new C();  
+		c.meow();  
+		c.eat();  
+		//c.bark();//C.T.Error  
+		}
+
+}
